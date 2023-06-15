029cfed6bd0d3dfb2da2b9a55db43e412b5d192c
khaleeque <kmem98@gmail.com>
2023-06-15 20:08:48 +0500

    changes

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -18,48 +18,12 @@ const getAccountData = () => {
     return JSON.parse(jsonData)   
 }
 
-// router.get("/appointments", (req, res) => {
-//     const accounts = getAccountData()
-//     res.status(200).json(accounts)
-//   });
-
-// router.post("/appointments", (req, res)=>{
-//     var existAccounts = getAccountData()
-//     const newAccountId = Math.floor(100000 + Math.random() * 900000)
- 
-//     existAccounts[newAccountId] = req.body
-   
-//     console.log('existAccounts ',existAccounts);
-//     saveAccountData(existAccounts);
-//     res.status(201).json({success: true, msg: 'appointment added successfully'})
-// });
-
-// router.put("/appointments/:id", (req, res) => {
-//     var existAccounts = getAccountData()
-//     fs.readFile(dataPath, 'utf8', (err, data) => {
-//       const accountId = req.params['id'];
-//       existAccounts[accountId] = req.body;
-//       saveAccountData(existAccounts);
-//       res.json(`appointment with id ${accountId} has been updated`)
-//     }, true);
-//   });
-
-// router.delete("/appointments/:id", (req, res) => {
-//     fs.readFile(dataPath, 'utf8', (err, data) => {
-//       var existAccounts = getAccountData()
-//       const userId = req.params['id'];
-//       delete existAccounts[userId]; 
-//       saveAccountData(existAccounts);
-//       res.json(`accounts with id ${userId} has been deleted`)
-//     }, true);
-//   });
-
-router.get("/", (req, res) => {
+router.get("/appointments", (req, res) => {
     const accounts = getAccountData()
     res.status(200).json(accounts)
   });
 
-router.post("/", (req, res)=>{
+router.post("/appointments", (req, res)=>{
     var existAccounts = getAccountData()
     const newAccountId = Math.floor(100000 + Math.random() * 900000)
  
@@ -70,7 +34,7 @@ router.post("/", (req, res)=>{
     res.status(201).json({success: true, msg: 'appointment added successfully'})
 });
 
-router.put("/:id", (req, res) => {
+router.put("/appointments/:id", (req, res) => {
     var existAccounts = getAccountData()
     fs.readFile(dataPath, 'utf8', (err, data) => {
       const accountId = req.params['id'];
@@ -80,7 +44,7 @@ router.put("/:id", (req, res) => {
     }, true);
   });
 
-router.delete("/:id", (req, res) => {
+router.delete("/appointments/:id", (req, res) => {
     fs.readFile(dataPath, 'utf8', (err, data) => {
       var existAccounts = getAccountData()
       const userId = req.params['id'];
