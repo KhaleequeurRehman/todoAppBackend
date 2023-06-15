1e8dccaf582d73b9cf01ad1cfc978e3f4f4e28de
khaleeque <kmem98@gmail.com>
2023-06-16 01:00:24 +0500

    changes

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const fs = require('fs');
+const path = require('path');
 var logger = require("morgan");
 require("dotenv").config();
 const fileupload = require('express-fileupload');
 const cors = require("cors");
 
+// const dataPath = './temp/appointment.json'
+const dataPath = path.join(__dirname,'../temp/appointment.json')
+
 // const indexRouter = require("./Routes/index");
 // const todoRoutes = require("./routes/todoRoute");
-const appointmentRoutes = require('./routes/appointmentRoutes')
+// const appointmentRoutes = require('./routes/appointmentRoutes')
 // const connectDb = require('./config/db')
 
 const app = express();
@@ -36,10 +41,58 @@ app.use(cors());
 // app.use("/", indexRouter);
 app.get("/", function(req, res) {
 	// res.send('TodoApp Backend is Running');
+	const accounts = getAccountData()
+    res.status(200).json({success:true,msg:'appointments fetched successfully',data:accounts})
 	res.send('App is Running');
 });
 
-app.use('/api/v1/', appointmentRoutes)
+// util functions
+const saveAccountData = (data) => {
+    const stringifyData = JSON.stringify(data)
+    fs.writeFileSync(dataPath, stringifyData)
+}
+const getAccountData = () => {
+    const jsonData = fs.readFileSync(dataPath)
+    return JSON.parse(jsonData)   
+}
+
+app.get("/appointments", (req, res) => {
+    const accounts = getAccountData()
+    res.status(200).json({success:true,msg:'appointments fetched successfully',data:accounts})
+  });
+
+app.post("/appointments", (req, res)=>{
+    var existAccounts = getAccountData()
+    const newAccountId = Math.floor(100000 + Math.random() * 900000)
+ 
+    existAccounts[newAccountId] = req.body
+   
+    console.log('existAccounts ',existAccounts);
+    saveAccountData(existAccounts);
+    res.status(201).json({success: true, msg: 'appointment added successfully'})
+});
+
+app.put("/appointments/:id", (req, res) => {
+    var existAccounts = getAccountData()
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+      const accountId = req.params['id'];
+      existAccounts[accountId] = req.body;
+      saveAccountData(existAccounts);
+      res.json({success:true, msg:`appointment with id ${accountId} has been updated`})
+    }, true);
+  });
+
+app.delete("/appointments/:id", (req, res) => {
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+      var existAccounts = getAccountData()
+      const userId = req.params['id'];
+      delete existAccounts[userId]; 
+      saveAccountData(existAccounts);
+      res.json({success:true, msg:`accounts with id ${userId} has been deleted`})
+    }, true);
+  });
+
+// app.use('/api/v1/', appointmentRoutes)
 // app.use('/api/v1/',todoRoutes);
 
 
